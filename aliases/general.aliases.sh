52f1a9cf6738337bb48552f485494d96f00c6955
Ken Ha <ken8waru1@gmail.com>
2024-08-01 03:14:38 -0700

    Add expressvpn, vim, and docker desktop aliases

--- aliases/general.aliases.sh
+++ aliases/general.aliases.sh
@@ -1,2 +1,10 @@
 alias less='less -R'
 alias ll='ls -alF'
+
+alias vpnc='expressvpn connect'
+alias vpndc='expressvpn disconnect'
+
+# alias nvim to vim
+alias vim='nvim'
+
+alias dockergui='/opt/docker-desktop/Docker\ Desktop'
