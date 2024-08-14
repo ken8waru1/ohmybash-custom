8a312f01ad9405e6f05904e74a14009d2c6d7aca
Ken Ha <ken8waru1@gmail.com>
2024-08-14 16:57:31 -0700

    Move aliases into new file

--- aliases/general.aliases.sh
+++ aliases/general.aliases.sh
@@ -1,10 +1,2 @@
 alias less='less -R'
 alias ll='ls -alF'
-
-alias vpnc='expressvpn connect'
-alias vpndc='expressvpn disconnect'
-
-# alias nvim to vim
-alias vim='nvim'
-
-alias dockergui='/opt/docker-desktop/Docker\ Desktop'
---
+++ aliases/tools.aliases.sh
@@ -0,0 +1,8 @@
+alias vpnc='expressvpn connect'
+alias vpndc='expressvpn disconnect'
+
+alias vim='nvim'
+
+alias dockergui='/opt/docker-desktop/Docker\ Desktop'
+
+alias rdw='rdict -w'
