541fc8d7ec04aaa624301c8929739abb4c07fee0
ken8waru1 <ken8waru1@gmail.com>
2026-03-23 22:19:01 -0700

    Update aliases

--- aliases/tools.aliases.sh
+++ aliases/tools.aliases.sh
@@ -1,8 +1,4 @@
-alias vpnc='expressvpn connect'
-alias vpndc='expressvpn disconnect'
-
 alias vim='nvim'
-
 alias dockergui='/opt/docker-desktop/Docker\ Desktop'
-
 alias rdw='rdict -w'
+alias lock='swaylock -c 1e1e2e'
